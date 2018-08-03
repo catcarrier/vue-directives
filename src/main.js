c502d12daf93775687b6043d1a530ae36023fdaf
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-08-03 17:16:36 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// directives have five hooks bind, inserted, update, componentUpdated, unbind
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor='green'
+    // el.style.backgroundColor=binding.value; /* v-highlight="'red'" */
+    var delay = 0;
+    if (binding.modifiers['delayed']) { delay = 3000 }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay)
+
+
+
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
